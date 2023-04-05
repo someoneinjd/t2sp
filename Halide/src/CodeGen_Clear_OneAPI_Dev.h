eddeeddebd2b98264b5ca5e28e82ecc8f70c1546
someoneinjd <someoneinjd@outlook.com>
2023-04-05 11:34:00 +0800

    Fix typo

--- Halide/src/CodeGen_Clear_OneAPI_Dev.h
+++ Halide/src/CodeGen_Clear_OneAPI_Dev.h
@@ -284,8 +284,8 @@ protected:
         void create_assertion(const std::string &id_cond, const std::string &id_msg);
 
     public:
-        EmitOneAPIFunc(CodeGen_Clear_OneAPI_C* parent, std::ostringstream &s, Target t) : CodeGen_Clear_OneAPI_C(s, t),
-            parent(parent) { // Note this assocates s with stream in CodeGen_Clear_OneAPI_C, which is inherited from IRPrinter
+        EmitOneAPIFunc(CodeGen_Clear_OneAPI_C* parent, std::ostringstream &s, Target t) :
+            CodeGen_Clear_OneAPI_C(s, t), parent(parent) { // Note this assocates s with stream in CodeGen_Clear_OneAPI_C, which is inherited from IRPrinter
                 stream_ptr = &s;
                 ext_funcs.p = this;
                 currently_inside_kernel = false;
